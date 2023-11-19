75d9f2ad67e9d474359ff8ab018f0a0f81e85ce2
Zygisvaldo <zygiodeze@gmail.com>
2023-11-19 13:09:56 +0100

    feat: auto logout implemented

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -2,6 +2,7 @@ import { Outlet, useLoaderData, useSubmit } from "react-router-dom";
 
 import MainNavigation from "../components/MainNavigation";
 import { useEffect } from "react";
+import { getTokenDuration } from "../util/auth";
 
 function RootLayout() {
   // const navigation = useNavigation();
@@ -12,10 +13,19 @@ function RootLayout() {
     if (!token) {
       return;
     }
+
+    // if token is expired, auto logout submitted
+    if (token === "EXPIRED") {
+      submit(null, { action: "/logout", method: "post" });
+      return;
+    }
+
     // if token start timer
+    const tokenDuration = getTokenDuration();
+    console.log(tokenDuration);
     setTimeout(() => {
       submit(null, { action: "/logout", method: "post" }); // manually subbmitting logout action that belogs to '/logout' route, with no data (null)
-    }, 1 * 60 * 60 * 1000); // 1h is ms
+    }, tokenDuration); // 1 * 60 * 60 * 1000); // 1h is ms
   }, [token, submit]);
 
   return (
--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -1,12 +1,32 @@
 import { redirect } from "react-router-dom";
 
 export function storeAuthToken(token) {
+  const expiration = new Date();
+  expiration.setHours(expiration.getHours() + 1); // 1h in the future
   localStorage.setItem("token", token);
+  localStorage.setItem("expiration", expiration.toISOString());
 }
 
 export function getAuthToken() {
   const token = localStorage.getItem("token");
+
+  if (!token) {
+    return null; // must NOT just 'return' becaus it will be undefined
+  }
+
+  const tokenDuration = getTokenDuration();
+  if (tokenDuration < 0) {
+    return "EXPIRED";
+  }
   return token;
+}
+
+export function getTokenDuration() {
+  const storedExpirationDate = localStorage.getItem("expiration");
+  const expirationDate = new Date(storedExpirationDate); // restore from ISOString to a new Date object
+  const now = new Date();
+  const duration = expirationDate.getTime() - now.getTime(); // .getTime() returns time in ms
+  return duration;
 }
 
 export function tokenLoader() {
